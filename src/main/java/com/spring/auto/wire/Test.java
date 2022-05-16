2a995c9b1d0d79d4ec31788991300e8153b31d5a
shankar87dayal <shankar.87.dayal@gmail.com>
2022-05-16 11:59:53 +0530

    Autowiring using XML complete Session byName, byType & byConstructor Spring

---
+++ src/main/java/com/spring/auto/wire/Test.java
@@ -0,0 +1,13 @@
+package com.spring.auto.wire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+ 
+	 ApplicationContext context=	new ClassPathXmlApplicationContext("com/spring/auto/wire/autoconfig.xml");
+	 Emp emp1 = context.getBean("emp1",Emp.class);
+	 System.out.println(emp1);
+	}
+}
