058c46a76016ca5f92965fa9fe404afcd579abe1
shankar87dayal <shankar.87.dayal@gmail.com>
2022-05-16 16:57:42 +0530

    Qualifier Annotation with Autowiring Spring

--- src/main/java/com/spring/auto/wire/annotation/Emp.java
+++ src/main/java/com/spring/auto/wire/annotation/Emp.java
@@ -1,9 +1,11 @@
 package com.spring.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
 //	@Autowired   // we can use this line  as a variable & output   (Emp [address=Address [street=alamaram, city=chennai]])
+	@Qualifier("address1")
 	private Address address;
 
 	public Address getAddress() {
