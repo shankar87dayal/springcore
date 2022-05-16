0d9aa1353114fdcbf9405377bc891d721abc3e93
shankar87dayal <shankar.87.dayal@gmail.com>
2022-05-16 10:02:00 +0530

    Implementing bean life cycle using interfaces IntializingBean DisposableBean

--- src/main/java/com/spring/lifecycle/Test.java
+++ src/main/java/com/spring/lifecycle/Test.java
@@ -6,11 +6,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/spring/lifecycle/life-c-config.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/spring/lifecycle/life-c-config.xml");
+
+		// registering shortdown hook method inside the AbstractApplicationContext
+		// interface
+		context.registerShutdownHook();
+
 		Samosa s1 = (Samosa) context.getBean("s1");
 		System.out.println(s1);
-		
-		//registering shortdown hook method inside the AbstractApplicationContext interface
-		context.registerShutdownHook();
+
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+		Pepsi p1 = (Pepsi) context.getBean("p1");
+		System.out.println(p1);
 	}
 }
