747dee6cccf64ab16728289aaae89a067d2eabf1
shankar87dayal <shankar.87.dayal@gmail.com>
2022-05-16 10:48:32 +0530

    Implementing Bean LifeCyle using Annotations @PostConstruct & @PreDestroy

--- src/main/java/com/spring/lifecycle/Test.java
+++ src/main/java/com/spring/lifecycle/Test.java
@@ -12,12 +12,15 @@ public class Test {
 		// registering shortdown hook method inside the AbstractApplicationContext
 		// interface
 		context.registerShutdownHook();
-
-		Samosa s1 = (Samosa) context.getBean("s1");
-		System.out.println(s1);
-
-		System.out.println("+++++++++++++++++++++++++++++++++++++++++");
-		Pepsi p1 = (Pepsi) context.getBean("p1");
-		System.out.println(p1);
+//
+//		Samosa s1 = (Samosa) context.getBean("s1");
+//		System.out.println(s1);
+//
+//		System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+//		Pepsi p1 = (Pepsi) context.getBean("p1");
+//		System.out.println(p1);
+		
+		Example example=	(Example) context.getBean("example");
+		System.out.println(example);
 	}
 }
