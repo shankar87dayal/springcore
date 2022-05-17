ac003d67767c77945180371415c1ea73c8b709e7
shankar87dayal <shankar.87.dayal@gmail.com>
2022-05-17 09:27:07 +0530

    By default provides the list is java.util.ArrayList.

--- src/main/java/com/spring/collections/Test.java
+++ src/main/java/com/spring/collections/Test.java
@@ -14,6 +14,7 @@ public class Test {
 	System.out.println(emp1.getPhones());
 	System.out.println(emp1.getAddresses());
 	System.out.println(emp1.getCourses());
+	System.out.println(emp1.getPhones().getClass().getName());
 	
 	}
 
